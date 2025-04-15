c924af4973d7ec6d24d909ef3c49a5be3f41b912
Phước Thành <thanhgithub123@gmail.com>
2025-04-15 22:33:43 +0700

    Cập nhật bổ sung kế hoạch readMe

--- router/product.router.js
+++ router/product.router.js
@@ -1,22 +1,12 @@
 const express = require("express");
-const {
-  getAllProduct,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-} = require("../controller/product.controller");
+const { getAllProduct, createProduct, updateProduct, deleteProduct } = require("../controller/product.controller");
 const { authenticateToken } = require("../middleware/authMiddleware");
-const {  upload } = require("../middleware/multer");
+const { upload } = require("../middleware/multer");
 
 const router = express.Router();
 
-router.post(
-  "/create",
-  authenticateToken,
-  upload.single("profilePicture"),
-  createProduct
-);
 router.get("/", getAllProduct);
+router.post("/create", authenticateToken, upload.single("profilePicture"), createProduct);
 router.put("/update/:id", authenticateToken, updateProduct);
 router.delete("/delete/:id", authenticateToken, deleteProduct);
 
